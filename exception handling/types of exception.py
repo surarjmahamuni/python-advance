38b2c15c8b2d5b141fbc2f4674ea14cbbb8fdd8d
suraj <job.surajmahamuni@gmail.com>
2026-04-02 02:14:08 -0700

    first

---
+++ exception handling/types of exception.py
@@ -0,0 +1,22 @@
+# Pre-defined Exceptions
+
+# print(int("ten"))  -->  Pre-defined Exception --> ValueError: invalid literal for int()
+
+# User-defined/Programmer/Custom Exception
+# defined by programmers to raise custom exception.
+
+class User:
+
+    def validate(self,age):
+        if age>=18 and age<110:
+            print("You are old enough")
+        else:
+            raise InvalidAge("Not eligible")
+
+class InvalidAge(Exception):
+
+    def __init__(self,msg):
+        self.msg=msg
+
+u=User()
+u.validate(112)  # User-defined exception --> InvalidAge: Not eligible
