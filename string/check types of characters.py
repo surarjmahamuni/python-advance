fcf92ae1c63fcea7bcdfc109644fe6ef8c7928c4
suraj <job.surajmahamuni@gmail.com>
2026-04-04 18:20:03 -0700

    first

---
+++ string/check types of characters.py
@@ -0,0 +1,65 @@
+# there are so many methods in string we can use to find what types of character the given string contains
+
+#isalpha()
+print("AbCdeF".isalpha())
+
+# isalnum()
+print("A1b3e".isalnum())
+
+#isdigit()
+print("1234".isdigit())
+
+#islower()
+print("baa".islower())
+print("123abc".islower())  # True
+
+#isupper()
+print("ABC".isupper())
+
+#istitle()
+print("The Title".istitle())
+
+#isspace()
+print("    ".isspace())
+
+#outputs:
+# True
+# True
+# True
+# True
+# True
+# True
+# True
+
+###
+
+#write a program to check type of character entered by the user
+
+#user input
+ch=input("Enter the character: ")
+
+#check if ch is alph-numeric
+if ch.isalnum():
+
+    #if alpha
+    if ch.isalpha():
+
+        #if uppercase
+        if ch.isupper():
+            print("Uppercase")
+
+        #if lowercase
+        else:
+            print("Lowercase")
+
+    #if digit
+    else:
+        print("digit")
+
+#is space
+elif ch.isspace():
+    print("space character")
+
+#special character
+else:
+    print("special character")
