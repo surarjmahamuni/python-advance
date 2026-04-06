ff9477dbc8680a4108d243f96699ee2c5926ac12
suraj <job.surajmahamuni@gmail.com>
2026-04-06 13:41:48 -0700

    first commit

---
+++ list/pop.py
@@ -0,0 +1,19 @@
+#remove()
+
+l=[10,20,30]
+print(l.pop())       # 30 --> removes the last element and returns it
+print(l)  # [10,20] last element is gone
+
+print(l.pop())  # 20
+print(l.pop())  # 10
+print(l)        # []
+
+# print(l.pop())  -> IndexError
+
+# we can also remove element from specified index
+l1=[10,20,30]
+
+print(l1.pop(1)) # 20
+print(l1)        # [10,30] --> element at index 1 is gone
+
+
