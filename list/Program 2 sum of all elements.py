07be4501873fed67dd0bac481d44bda77c503b5f
suraj <job.surajmahamuni@gmail.com>
2026-04-06 20:52:45 -0700

    first commit

---
+++ list/Program 2 sum of all elements.py
@@ -0,0 +1,14 @@
+# write a program to get the sum of all the elements present in the list
+
+#user input
+list1=[10,20,30]
+
+#core logic
+
+total=0
+#traverse over each element in the list
+for i in list1:
+
+    total = total + i
+
+print(total)  # o/p: 60
---
+++ list/Program 3 Max number in the list.py
@@ -0,0 +1,14 @@
+# write a program to find the largest number present in the list
+
+#user input:
+list1=[100,300,2,900]
+
+#core logic:
+max_num=list1[0]
+
+#traverse the list
+for num in list1:
+    if num>max_num:
+        max_num=num
+
+print(max_num)  # o/p: 900
