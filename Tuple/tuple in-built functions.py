2945672652a50bc3ac63b43be7b9f2f5f0a6abf0
suraj <job.surajmahamuni@gmail.com>
2026-04-07 15:42:35 -0700

    first commit

---
+++ Tuple/tuple in-built functions.py
@@ -0,0 +1,17 @@
+### len()
+
+t1=(10,20,30)
+print(len(t1))  # 3
+
+### count()
+
+print(t1.count(10)) # 1
+print(t1.count(100))  # 0 --> not present
+
+### index()
+
+print()
+print(t1.index(10)) # 1
+#print(t1.index(100)) # ValueError
+print(t1.index(20,1,10))  # 1
+
