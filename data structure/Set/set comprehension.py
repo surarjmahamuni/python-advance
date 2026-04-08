04f250d61b05a6eb61345eca444d3b697b12f72d
suraj <job.surajmahamuni@gmail.com>
2026-04-08 15:42:24 -0700

    first commit

---
+++ data structure/Set/set comprehension.py
@@ -0,0 +1,6 @@
+s1= {x*x for x in range(1,6)}  # --> square of each element from 1 to 5
+print(s1)  # {1, 4, 9, 16, 25}
+
+s2={2**x for x in range(1,6)}  # --> 2 to the power of element
+print(s2)  # {32, 2, 4, 8, 16}
+
