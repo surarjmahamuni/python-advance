db8133f528dafdfdb0b5630de4b2ab97d234c9b1
suraj <job.surajmahamuni@gmail.com>
2026-04-10 15:40:03 -0700

    refactor: added search functionality to look up student record

--- data structure/Dictionary/Programs/student_management_application.py
+++ data structure/Dictionary/Programs/student_management_application.py
@@ -23,6 +23,31 @@ def display_student_records(student_record):
     for name,marks in student_record.items():
         print("{} has {} marks".format(name,marks))
 
+def search_student_records(student_record):
+    """
+    Search student record in the dictionary
+    Args:
+    student_record {dict}: student record
+    Return:
+    None
+    """
+
+    while True:
+
+        student_name=input("Enter the student name: ")
+        student_marks=student_record.get(student_name,-1)
+
+        if student_marks==-1:
+            print("Student doesnt exist in the records.")
+
+        else:
+            print(f"{student_name} marks are {student_marks}")
+
+        option=input("Do you want to continue (yes/no)").lower()
+        if option=='no':
+            break
+
+
 # User input
 number=int(input("Enter how many numbers student: "))
 
@@ -31,3 +56,6 @@ student_records=add_student_records(number)
 
 # Display Records
 display_student_records(student_records)
+
+# Search record
+search_student_records(student_records)
