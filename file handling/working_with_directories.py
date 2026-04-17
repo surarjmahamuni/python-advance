0feaa6530e4bdddb6be0494ff8952fc686386d22
suraj <job.surajmahamuni@gmail.com>
2026-04-17 16:27:02 -0700

    feat: wrote a function to work with directories.

--- file handling/working_with_directories.py
+++ file handling/working_with_directories.py
@@ -5,19 +5,38 @@ def directory_functions():
 
     # Current working directory
     cwd=os.getcwd()
-    print(f'Current working directory is: {cwd}')
+    print(f'Current working directory is: {cwd}',end='\n')
 
     # Create a directory in the current directory
     os.mkdir('dir1')
-    print("directory is created.")
+    print("directory is created.\n")
 
     # create directory inside directory from current directory
     os.mkdir('dir1/dir2')
-    print('Successfully created sub-directory')
+    print('Successfully created sub-directory\n')
 
     # To create multiple directories:
     os.makedirs('dir2/dir3/dir4')
-    print('Successfully created sub-directories')
+    print('Successfully created directory and sub-directories\n')
+
+    # To rename the directory:
+    os.rename('dir2/dir3/dir4','dir2/dir3/dir5')
+    print('Renamed the directory \n')
+
+    # To print the content of current directory:
+    dir_list=os.listdir('C:\\Users\\srmah\\PycharmProjects\\python-advance\\file handling')
+    print(f'Content in the current directory:{dir_list}\n')
+
+    # To print out the content of current and its sub-directories
+    all_dir=os.walk('C:\\Users\\srmah\\PycharmProjects\\python-advance\\file handling')
+    print(f'Current working directory and its sub-directories: {all_dir}\n')
+
+    # display the content of current and sub-directories
+    for dirpath,dirnames,filenames in all_dir:
+        print(f'Directory path: {dirpath}')
+        print(f'sub-directories: {dirnames}')
+        print(f'filenames: {filenames}')
+        print()
 
     # To remove directory or sub-directory
     os.rmdir('dir1/dir2')
@@ -25,7 +44,7 @@ def directory_functions():
     os.rmdir('dir1')
 
     # To remove multiple directories in the path
-    os.removedirs('dir2/dir3/dir4')
+    os.removedirs('dir2/dir3/dir5')
     print('Successfully removed all sub-directories')
 
 
